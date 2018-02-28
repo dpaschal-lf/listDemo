966fda58b858144fdfd7bb864f1dcab49edcbf99
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-02-28 15:04:52 -0800

    skeleton for functions

--- main.js
+++ main.js
@@ -1,9 +1,61 @@
-
+$(document).ready( initializeApp) ;
 
 var staff = [
-	{
-		name: ,
-		occupation: ,
-		avatar:
-	}
-];
+  {
+    name: 'Larry',
+    occupation: 'Salesman',
+    avatar: 'https://c532f75abb9c1c021b8c-e46e473f8aadb72cf2a8ea564b4e6a76.ssl.cf5.rackcdn.com/2016/08/31/2gfb850rm6_Julie_Larsen_Maher_0100_California_Sea_Lion_Indy_close_up_SLP_BZ_03_08_12_hr.jpg'
+  },
+  {
+    name: ,
+    occupation: ,
+    avatar:
+  },
+  {
+    name: 'Goldie',
+    occupation:'Carrot Farmer',
+    avatar: 'http://www.dogtrouble.co.uk/wp-content/uploads/2017/07/Dog-Health-Products-500x500.jpg'
+  },
+  {
+    name: 'Tony',
+    occupation: 'Future Cereal Spokesperson',
+    avatar:'https://www.vallartadaily.com/wp-content/uploads/2017/08/bengal-tiger-cub-35900.jpg'
+  },
+  {
+    name: 'Albert',
+    occupation: 'saving the world',
+    avatar: 'https://images-na.ssl-images-amazon.com/images/I/5139%2Bw4F5iL._US500_.jpg'
+  },
+  {
+    name: 'Newt',
+    occupation: 'HR Supervisor',
+    avatar: 'https://files.slack.com/files-pri/T1EHQUJ8J-F9GMJKN93/image.png'
+  },
+  {
+    name: 'Finny',
+    occupation: 'Professional Jumper',
+    avatar: 'https://www.dolphindiscovery.com/imagesNuevo/imgsLocs/anguilla/anguilla-9.jpg?v=50'
+  },
+  {
+    name: 'Jimmy',
+    occupation: 'Cleaning Small Rocks',
+    avatar: 'http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/6_maja_06_r._ZOO_204.jpg/500px-6_maja_06_r._ZOO_204.jpg'
+  },
+
+  ]
+//render all staff on page load
+function initializeApp(){
+
+}
+// go through each staff member and call renderOneStaff, passing in the staff member data
+function renderAllStaff( allStaff ){
+
+}
+//create the dom elements for the staff member and put it in the #staffList.  use the existing html as your guide for how it should be formatted
+// add a click handler to the parent element for each staff member that changes the details in the #staffDetails element, according to the example there
+function renderOneStaff( member ){
+
+}
+
+
+
