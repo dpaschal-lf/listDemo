5ba6701ba600a9f29ba8f256129aead405f00a91
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-02-28 17:34:13 -0800

    completed code

--- main.js
+++ main.js
@@ -40,17 +40,91 @@ var staff = [
   ]
 //render all staff on page load
 function initializeApp(){
-
+	renderAllStaff(staff);
+	showStaffDetails( staff[0] );
 }
 // go through each staff member and call renderOneStaff, passing in the staff member data
 function renderAllStaff( allStaff ){
-
+	for(var indexOfStaffMember=0; indexOfStaffMember<allStaff.length; indexOfStaffMember++){
+		var staffMember = allStaff[ indexOfStaffMember ];
+		renderOneStaff( staffMember );
+	}
 }
 //create the dom elements for the staff member and put it in the #staffList.  use the existing html as your guide for how it should be formatted
 // add a click handler to the parent element for each staff member that changes the details in the #staffDetails element, according to the example there
 function renderOneStaff( member ){
+	var staffContainer = $("<div>" , {
+		'class': 'staffMember',
+		on: {
+			click: function(){
+				console.log(member);
+				showStaffDetails(member);
+			}
+		}
+	})
+	staffContainer[0].STAFFmemberSTAFF = member;
+	var staffName = $("<div>",{
+		'class': 'name',
+		text: member.name
+	})
+	var staffOccupation = $("<div>",{
+		'class': 'occupation',
+		text: member.occupation
+	})
+	staffContainer.append(staffName, staffOccupation);
+	$("#staffList").append( staffContainer );
 
 }
+/*
+		<img class="avatar" src="http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/6_maja_06_r._ZOO_204.jpg/500px-6_maja_06_r._ZOO_204.jpg"></img>
+		<div class="name">Jimmy</div>
+		<div class="occupation">Cleaning Small Rocks</div>
+	  name: 'Jimmy',
+    occupation: 'Cleaning Small Rocks',
+    avatar: 'http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/6_maja_06_r._ZOO_204.jpg/500px-6_maja_06_r._ZOO_204.jpg'
+
+		*/
+function showStaffDetails( member) {
+	$("#staffDetails > .detailStaff").empty();
+	var staffName = $("<div>",{
+		'class': 'name',
+		text: member.name
+	})
+	var staffOccupation = $("<div>",{
+		'class': 'occupation',
+		text: member.occupation
+	})	
+	var staffAvatar = $("<img>",{
+		'class': 'avatar',
+		src: member.avatar,
+	});
+	$("#staffDetails > .detailStaff").append(staffAvatar, staffName, staffOccupation);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
